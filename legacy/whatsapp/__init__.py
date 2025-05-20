1b636fc0c1f346ce318cf6258bcef47e104e1568
bastianibanez <bastian.miba@gmail.com>
2025-05-20 17:34:30 -0400

    Modified imports in init

--- legacy/whatsapp/__init__.py
+++ legacy/whatsapp/__init__.py
@@ -1,8 +1,8 @@
-from src.external_integrations.whatsapp.api_whatsapp import whatsapp_service
-from src.external_integrations.whatsapp.messages import WhatsAppMessages, whatsapp_messages
-from src.external_integrations.whatsapp.media import WhatsAppMedia, whatsapp_media
-from src.external_integrations.whatsapp.client import WhatsAppClient, whatsapp_client
-from src.external_integrations.whatsapp.models import (
+from .api_whatsapp import whatsapp_service
+from .messages import WhatsAppMessages, whatsapp_messages
+from .media import WhatsAppMedia, whatsapp_media
+from .client import WhatsAppClient, whatsapp_client
+from .models import (
     WhatsAppTextMessage,
     WhatsAppImageMessage,
     WhatsAppMediaResponse,
