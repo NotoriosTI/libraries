9c33ddf2099c3bf6550a48436491b1c690567985
bastianibanez <bastian.miba@gmail.com>
2025-05-29 13:51:40 -0400

    Added temporary legacy support with deprecation warnign

---
+++ odoo-api/src/odoo_api/product_v2.py
@@ -0,0 +1,12 @@
+import warnings
+from product import *
+
+warnings.warn(
+"""
+product_v2.py ahora es product.py
+Asegurate de arreglar este import ya que en el futuro
+product_v2.py dejara de existir.
+""",
+DeprecationWarning,
+stacklevel=2
+)
