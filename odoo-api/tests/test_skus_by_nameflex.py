b4ff838225adaccbcfc0d3a0b15c350115aacd2a
NotoriosTI <ti@notorios.cl>
2025-05-30 20:19:27 -0400

    Test modificado

--- odoo-api/tests/test_skus_by_nameflex.py
+++ odoo-api/tests/test_skus_by_nameflex.py
@@ -6,13 +6,10 @@ odoo_product = OdooProduct(
     dotenv_path="/Users/bastianibanez/work/libraries/odoo-api/.env"
     )
 
-odoo_warehouse = OdooWarehouse(
-    database="test",
-    dotenv_path="/Users/bastianibanez/work/libraries/odoo-api/.env"
-)
+
 
 resultados = odoo_product.get_skus_by_name_flexible("aceite de argan")
 for i, (nombre, sku) in enumerate(resultados[:]):
-    var_attrs = odoo_warehouse.get_variant_attributes_by_sku(sku)
+    var_attrs = odoo_product.get_variant_attributes_by_sku(sku)
     resultados[i] = (nombre, sku, var_attrs)
     print(f"Nombre: {nombre} | SKU: {sku} | Formato: {var_attrs}")
