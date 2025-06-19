f6b347873ab0987d214ff0487a7df250656d8862
NotoriosTI <ti@notorios.cl>
2025-06-19 11:28:39 -0400

    Stock progress

--- odoo-api/src/odoo_api/__init__.py
+++ odoo-api/src/odoo_api/__init__.py
@@ -1,3 +1,39 @@
 from .product import OdooProduct
 
+__all__ = ['OdooProduct']
+
+
+
+
+    
+
+
+
+# def test(): 
+    
+#     odoo_api = product.OdooProduct(database='test', dotenv_path='.env')
+
+#     name_or_sku = "aceite de coco"
+    
+#     if odoo_api.product_exists(name_or_sku):
+#         # Direct order
+#         pass
+
+#     flexible_search = odoo_api.get_skus_by_name_flexible(name_or_sku)
+
+#     if not flexible_search:
+#         # Error
+#         pass
+
+#     if len(flexible_search) == 1:
+#         # Direct order
+#         pass
+
+#     if len(flexible_search) > 1:
+#         # Interrupt and select
+#         pass
+
+
+# if __name__ == "__main__":
+#     test()
 
---
+++ odoo-api/tests/stock_movements.py
@@ -0,0 +1,16 @@
+from src.odoo_api import OdooProduct
+
+def test():
+    pass
+
+if __name__ == "__main__":
+    test()
+
+
+
+
+
+
+
+
+
