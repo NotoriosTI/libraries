8d0a92785e9dfe2ab42ddea59eef7ed668310eec
bastianibanez <bastian.miba@gmail.com>
2025-07-09 23:19:33 -0400

    Prod-order debug

--- odoo-api/src/odoo_api/product.py
+++ odoo-api/src/odoo_api/product.py
@@ -268,6 +268,7 @@ class OdooProduct(OdooAPI):
         """
         Verifica si el producto ya existe en Odoo basándose en el SKU y que el producto esté habilitado para la venta (sale_ok=True).
         """
+        print(f"[DEBUG-product_exists] sku: {sku}")
         product_ids = self.models.execute_kw(
             self.db, self.uid, self.password,
             'product.product', 'search',
