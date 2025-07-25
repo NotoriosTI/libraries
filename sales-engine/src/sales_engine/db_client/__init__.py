024056ebe69d25c1896e6f26f1687cb0780d64a4
bastianibanez <bastian.miba@gmail.com>
2025-07-25 17:21:43 -0400

    Implemented db reader for the forecast table on salesdb

--- sales-engine/src/sales_engine/db_client/__init__.py
+++ sales-engine/src/sales_engine/db_client/__init__.py
@@ -6,5 +6,6 @@ Módulo simplificado para leer datos de la base de datos de ventas.
 
 from sales_engine.db_client.db_reader import DatabaseReader
 from .query_builder import QueryBuilder
+from .forecast_reader import ForecastReader, get_forecasts_by_month
 
-__all__ = ['DatabaseReader', 'QueryBuilder'] 
+__all__ = ['DatabaseReader', 'QueryBuilder', 'ForecastReader', 'get_forecasts_by_month'] 
