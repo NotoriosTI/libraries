4baa12d8058817712bd71cc5787c31018291ab21
bastianibanez <bastian.miba@gmail.com>
2025-10-11 18:59:37 -0300

    Updated test

--- config-manager/tests/test_secrets.py
+++ config-manager/tests/test_secrets.py
@@ -6,9 +6,10 @@ from config_manager.juan import JuanDBSecret
 if __name__ == "__main__":
     odoo_secrets = OdooProductionSecret()
     emiliadb_secrets = EmiliaDBSecret()
+    juandb_secrets = JuanDBSecret()
     chatwoot_secrets = ChatwootSecret()
 
     print(f"{odoo_secrets.db = }")
     print(f"{emiliadb_secrets.host = }")
-    print(f"{emiliadb_secrets.host = }")
+    print(f"{juandb_secrets.host = }")
     print(f"{chatwoot_secrets.account_id = }")
