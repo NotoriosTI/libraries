f81c7ec7bf28ab2d316b9fd67bbe5fe0a69888cf
bastianibanez <bastian.miba@gmail.com>
2025-10-13 19:42:27 -0300

    Changed init order

--- config-manager/src/config_manager/__init__.py
+++ config-manager/src/config_manager/__init__.py
@@ -3,12 +3,12 @@ Centralized configuration and secret management for all services.
 """
 
 from config_manager.settings import secrets
-from config_manager import common, juan, emma, emilia
+from config_manager import juan, emma, emilia, common
 
 __all__ = [
-    "secrets",
     "juan",
     "emma",
     "emilia",
     "common",
+    "secrets",
 ]
