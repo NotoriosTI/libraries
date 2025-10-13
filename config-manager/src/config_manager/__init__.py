8c3afd9d5cde82fbee0b3a29796d2deb78e46a49
bastianibanez <bastian.miba@gmail.com>
2025-10-13 15:14:20 -0300

    Re-added secrets to __all__ for back compatibility. Also added juan, common, emma and emilia

--- config-manager/src/config_manager/__init__.py
+++ config-manager/src/config_manager/__init__.py
@@ -1,3 +1,14 @@
 """
 Centralized configuration and secret management for all services.
 """
+
+from config_manager.settings import secrets
+from config_manager import common, juan, emma, emilia
+
+__all__ = [
+    "secrets",
+    "common",
+    "juan",
+    "emma",
+    "emilia",
+]
