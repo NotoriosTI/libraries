2137f6a497425a2dfa695eb6cca14234a407263d
1aravena <vichoaravenas186@gmail.com>
2025-10-22 12:10:08 -0300

    config-manager: add emma AnthropicSecret

--- config-manager/src/config_manager/emma.py
+++ config-manager/src/config_manager/emma.py
@@ -6,6 +6,9 @@ from pydantic import Field
 class OpenAISecret(Secret):
     api_key: Optional[str] = Field(default=None, alias="EMMA_OPENAI_API_KEY")
 
+class AnthropicSecret(Secret):
+    api_key: Optional[str] = Field(default=None, alias="EMMA_ANTHROPIC_API_KEY")
+
 class EnvironmentSecret(Secret):
     emma_environment: Optional[str] = Field(default=None, alias="EMMA_ENVIRONMENT")
 
