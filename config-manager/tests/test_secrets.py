1d1b0184d5b0c7421255d3eb9c5d62e161e2eb33
bastianibanez <bastian.miba@gmail.com>
2025-10-22 13:15:58 -0300

    Removed key printing

--- config-manager/tests/test_secrets.py
+++ config-manager/tests/test_secrets.py
@@ -4,13 +4,4 @@ from config_manager.emma import ChatwootSecret
 from config_manager.juan import JuanDBSecret
 
 if __name__ == "__main__":
-    juandb_secrets = JuanDBSecret()
-    # odoo_secrets = OdooProductionSecret()
-    # emiliadb_secrets = EmiliaDBSecret()
-    # chatwoot_secrets = ChatwootSecret()
-
-    print(f"{juandb_secrets.host = }")
-    print(f"{juandb_secrets.name = }")
-    print(f"{juandb_secrets.username = }")
-    print(f"{juandb_secrets.password = }")
-    print(f"{juandb_secrets.port = }")
+    pass
