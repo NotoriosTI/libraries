ea8099950a1118128e3aae26a721f142d764cc8d
bastianibanez <bastian.miba@gmail.com>
2025-11-27 08:53:32 -0300

    Created supply.py for rfq creation and tested

--- odoo-api/src/odoo_api/__init__.py
+++ odoo-api/src/odoo_api/__init__.py
@@ -4,6 +4,7 @@ from .journal import OdooJournal
 from .partners import OdooPartner
 from .product import OdooProduct
 from .sales import OdooSales
+from .supply import OdooSupply
 from .warehouses import OdooWarehouse
 
-__all__ = ['OdooAccountability', 'OdooCRM', 'OdooJournal', 'OdooPartner', 'OdooProduct', 'OdooSales', 'OdooWarehouse']
+__all__ = ['OdooAccountability', 'OdooCRM', 'OdooJournal', 'OdooPartner', 'OdooProduct', 'OdooSales', 'OdooSupply', 'OdooWarehouse']
