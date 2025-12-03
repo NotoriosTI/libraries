3ab3d5a98a565f8e63c6d23c5267ca93aee9d5ae
bastianibanez <bastian.miba@gmail.com>
2025-12-03 13:45:14 -0300

    Added get_partner_by_name

--- odoo-api/src/odoo_api/partners.py
+++ odoo-api/src/odoo_api/partners.py
@@ -73,6 +73,33 @@ class OdooPartner(OdooAPI):
         except Exception as e:
             print(f"Unknown error when parsing partner object: {e}")
 
+    def get_partner_by_name(self, name) -> Partner:
+        if not name:
+            raise ValueError("Name required")
+
+        partners = self.models.execute_kw(
+            self.db,
+            self.uid,
+            self.password,
+            "res.partner",
+            "search_read",
+            [[["name", "=ilike", name]]],
+            {
+                "fields": ["id", "name", "email", "customer_rank", "supplier_rank"],
+                "limit": 1,
+            },
+        )
+        if not partners:
+            return None
+
+        selected_partner = partners[0]
+
+        try:
+            partner_object = Partner(**selected_partner)
+            return partner_object
+        except Exception as e:
+            print(f"Unknown error when parsing partner object: {e}")
+
     def is_customer(self, email):
         partner = self.get_partner_by_email(email)
         return bool(partner and partner.customer_rank > 0)
